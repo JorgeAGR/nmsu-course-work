5e88b905e7fc8d987ae064683d46dc3de7eab9c3
Jorge A. Garcia <jorgeagr@localhost.localdomain>
2018-01-31 13:16:37 -0700

    Physics

--- PHYS315L/geigercounter.py
+++ PHYS315L/geigercounter.py
@@ -9,12 +9,28 @@ def xlsColumnData(xlssheet, min, max, column):
         array.append(d)
     return array
 
-datawb = openpyxl.load_workbook('Geigercounter.xlsx')
+dataWB = openpyxl.load_workbook('Geigercounter.xlsx')
 
-datasheet = datawb.get_sheet_by_name('Geigercounter')
+dataSheet = dataWB.get_sheet_by_name('Geigercounter')
 
-control = xlsColumnData(datasheet,1,101,2)
+controlData = xlsColumnData(dataSheet,1,101,2)
 
-measurement = xlsColumnData(datasheet,1,101,4)
+measurementData = xlsColumnData(dataSheet,1,101,4)
 
-f = 
+sampleSize = range(1,len(measurementData)+1)
+
+f, ax = plt.subplots(1, 2)
+
+#Displays graphs of experimental data (sample vs counts)
+ax[0].plot(sampleSize, controlData, 'o')
+ax[0].set_xlim(0,100)
+ax[0].set_ylim(0,6)
+ax[0].set_xticks(range(0,100,10))
+ax[0].grid()
+
+ax[1].plot(sampleSize, measurementData, 'o')
+ax[1].set_xlim(0,100)
+ax[1].set_ylim(0,100)
+ax[1].set_yticks(range(0,110,10))
+ax[1].set_xticks(range(0,100,10))
+ax[1].grid()
