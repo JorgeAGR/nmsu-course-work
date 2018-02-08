08ec8ed866dba38623200dd924188c2b5c61cb27
Jorge A. Garcia <jorgeagr97@gmail.com>
2018-02-07 17:37:03 -0700

    Physics

--- PHYS315L/Atomic-Spectroscopy/atomicspec.py
+++ PHYS315L/Atomic-Spectroscopy/atomicspec.py
@@ -1,36 +1,42 @@
 import numpy as np
 from uncertainties import ufloat
 import uncertainties.umath as umath
+import matplotlib.pyplot as plt
 
-def shiftTheta(measurement):
-    shift = measurement['blue']
+def rydberg():
+    None
+    
+
+def shiftTheta(measurement, center):
+    shift = measurement[center]
     for i in measurement:
         measurement[i] = np.abs(measurement[i] - shift)
 
-def avgTheta(measurement):
-    tv = []
-    tg = []
-    ty = []
-    to = []
+def avgTheta(measurement, color1, color2, color3, color4):
+    t1 = []
+    t2 = []
+    t3 = []
+    t4 = []
     for m in measurement:
-        tv.append(m['violet'])
-        tg.append(m['green'])
-        ty.append(m['yellow'])
-        to.append(m['orange'])
-    v = ufloat(np.mean(tv), np.std(tv))
-    g = ufloat(np.mean(tg), np.std(tg))
-    y = ufloat(np.mean(ty), np.std(ty))
-    o = ufloat(np.mean(to), np.std(to))
-    return v, g, y, o
+        t1.append(m[color1])
+        t2.append(m[color2])
+        t3.append(m[color3])
+        t4.append(m[color4])
+    c1 = ufloat(np.mean(t1), np.std(t1))
+    c2 = ufloat(np.mean(t2), np.std(t2))
+    c3 = ufloat(np.mean(t3), np.std(t3))
+    c4 = ufloat(np.mean(t4), np.std(t4))
+    return c1, c2, c3, c4
 
 def wavelength(d, theta, m):
     t = umath.radians(theta)
-    l = d * umath.sin(t) / m
-    return l
+    sint = umath.sin(t)
+    l = d * sint / m
+    return l, sint
+
+d = 1/(600 / (10**(-3))) # "Slit Size" m
 
 # == Mercury == #
-
-d = 1/(600 / (10**(-3))) # meters
 mHg = 1
 
 measurementsHg = [
@@ -43,21 +49,50 @@ measurementsHg = [
 ]
 
 for m in measurementsHg:
-    shiftTheta(m)
+    shiftTheta(m, 'blue')
 
-theta_vHg_avg, theta_gHg_avg, theta_yHg_avg, theta_oHg_avg = avgTheta(measurementsHg)
+theta_vHg_avg, theta_gHg_avg, theta_yHg_avg, theta_oHg_avg = avgTheta(measurementsHg, 'violet', 'green', 'yellow', 'orange')
 
-vHg = wavelength(d, theta_v_avg, mHg)
-gHg = wavelength(d, theta_g_avg, mHg)
-yHg = wavelength(d, theta_y_avg, mHg)
-oHg = wavelength(d, theta_o_avg, mHg)
+vHg, sintvHg = wavelength(d, theta_vHg_avg, 1)
+gHg, sintgHg = wavelength(d, theta_gHg_avg, 1)
+yHg, sintyHg = wavelength(d, theta_yHg_avg, 1)
+oHg, sintoHg = wavelength(d, theta_oHg_avg, 1)
+
+sintheta = [sintvHg.n, sintgHg.n, sintyHg.n, sintoHg.n]
+mlambda = [vHg.n, gHg.n, yHg.n, oHg.n]
+
+figHg, axHg = plt.subplots()
+axHg.errorbar(sintheta, mlambda, fmt = 'o')
 
 # == Hydrogren == #
+mH = 1
 
 measurementsH = [
 {'pink': 139.5, 'violet': 124.2, 'cyan': 122.2, 'red': 116.0, 'blue': 103.4}, # Angle Patrick Right
-{'pink': , 'violet': , 'cyan': , 'red': , 'blue': }, # Angle Patrick Left
-{'pink': , 'violet': , 'cyan': , 'red': , 'blue': }, # Angle Jorge Right
-{'pink': , 'violet': , 'cyan': , 'red': , 'blue': }, # Angle Jorge Left
+{'pink': 139.5, 'violet': 154.4, 'cyan': 156.2, 'red': 162.5, 'blue': 174.9}, # Angle Patrick Left
+{'pink': 139.3, 'violet': 124.1, 'cyan': 122.3, 'red': 116.0, 'blue': 103.3}, # Angle Jorge Right
+{'pink': 139.3, 'violet': 154.4, 'cyan': 156.3, 'red': 162.5, 'blue': 174.8}, # Angle Jorge Left
 {'pink': 139.3, 'violet': 124.2, 'cyan': 122.3, 'red': 116.0, 'blue': 103.4}, # Angle Ryan Right
 {'pink': 139.3, 'violet': 154.5, 'cyan': 156.3, 'red': 162.5, 'blue': 174.9}, # Angle Ryan Left
+]
+
+for m in measurementsH:
+    shiftTheta(m, 'pink')
+
+theta_vH_avg, theta_cH_avg, theta_rH_avg, theta_bH_avg = avgTheta(measurementsH, 'violet', 'cyan', 'red', 'blue')
+
+vH, sintvH = wavelength(d, theta_vH_avg, 1)
+cH, sintcH = wavelength(d, theta_cH_avg, 1)
+rH, sintrH = wavelength(d, theta_rH_avg, 1)
+bH, sintbH = wavelength(d, theta_bH_avg, 2)
+
+# == Uknown A == #
+'''
+measurementsA = [
+{'center': , 'color1': , 'color2': , 'color3': , 'color4': }, # Angle Patrick Right
+{'center': , 'color1': , 'color2': , 'color3': , 'color4': }, # Angle Patrick Left
+{'center': , 'color1': , 'color2': , 'color3': , 'color4': }, # Angle Jorge Right
+{'center': , 'color1': , 'color2': , 'color3': , 'color4': }, # Angle Jorge Left
+{'center': , 'color1': , 'color2': , 'color3': , 'color4': }, # Angle Ryan Right
+{'center': , 'color1': , 'color2': , 'color3': , 'color4': }, # Angle Ryan Left
+'''
