2b2a1c1a8e7e32583b60342a252af7d8ccf6c171
Jorge A, Garcia <jorgeagr97@gmail.com>
2018-03-01 16:56:02 -0700

    NMSU

--- ASTR401/HW4.py
+++ ASTR401/HW4.py
@@ -1,28 +1,41 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 27 14:12:12 2018
-
-@author: jorgeagr
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams['font.size'] = 15
+mpl.rcParams['figure.titlesize'] = 'medium'
+mpl.rcParams['legend.fontsize'] = 'small'
+mpl.rcParams['figure.figsize'] = [9.0, 9.0]
 
 def prob2(tau, N):
     I = [1]
-    for n in range(1,N):
-        i = I[-1] * np.exp(-tau)
-        I.append(i)
-    return I
+    dt = tau/N
+    for n in range(1,N):#range(1,N+1):
+        iout = I[-1] - I[-1]*dt
+        I.append(iout)
+    return np.array(I), I[-1]
+
+def func(tau):
+    iratio = np.exp(-tau)
+    return iratio
 
 tau = np.array([0.1, 1, 10])
 N = 1000
 
-I1 = prob2(tau[0], N)
-I2 = prob2(tau[1], N)
-I3 = prob2(tau[2], N)
+I1, end1= prob2(tau[0], N)
+I2, end2 = prob2(tau[1], N)
+I3, end3 = prob2(tau[2], N)
 
-plt.plot(range(N),I1)
-plt.plot(range(N),I2)
-plt.plot(range(N),I3)
+fig, ax = plt.subplots()
+ax.plot(range(N),I1)#, label = r'$\tau = 0.1$')
+ax.plot(range(N),I2, '-.')#, label = r'$\tau = 1$')
+ax.plot(range(N),I3, '--')#, label = r'$\tau = 10$')
+ax.plot((1000,1000,1000), func(tau), 'o', markersize = 10, label = r'$I/I_0=e^{-\tau}$')
+ax.set_xlim(0, N)
+ax.set_ylabel('Fraction Transmitted')
+ax.set_xlabel('Layer #')
+ax.grid()
+ax.legend()
+ax.annotate(r'$\tau = 0.1$', xy=(600, 0.95), xytext=(600, 0.95))
+ax.annotate(r'$\tau = 1$', xy=(500, 0.62), xytext=(500, 0.62))
+ax.annotate(r'$\tau = 10$', xy=(400, 0.03), xytext=(400, 0.03))
