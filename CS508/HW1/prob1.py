72baafbffeeb2cb9365480a724469b967cbe63ea
Jorge Garcia <jorgeagr@pop-os.localdomain>
2019-09-18 23:17:02 -0600

    Update

--- CS508/HW1/prob1.py
+++ CS508/HW1/prob1.py
@@ -11,10 +11,11 @@ import numpy as np
 import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from sklearn.decomposition import PCA
 
 golden_ratio = (np.sqrt(5) + 1) / 2
-width = 12
+width = 15
 height = width / golden_ratio
 
 mpl.rcParams['figure.figsize'] = (width, height)
@@ -29,48 +30,86 @@ mpl.rcParams['ytick.minor.size'] = 8
 mpl.rcParams['ytick.labelsize'] = 14
 
 with open('prob1_output.txt', 'w+') as file:
-
-    data = pd.read_csv('dataR2.csv')
-    data = data.round(3)
-    data_nc = data[data.keys()[:-1]]
+    # Load data onto pandas
+    data = pd.read_csv('data/dataR2.csv')
+    data_nc = data[data.keys()[:-1]] # Subset of data without 'Classification'
+    # Calculate the avg, std, min, max and percentiles of data
     data_desc = data_nc.describe().round(3)
+    print('Problem 1a: Data Satistics (Avg, Std, Min & Max)', file=file)
     print(data_desc.to_string(), end='\n\n', file=file)
-        
+    
+    # Calculate the covariance pairs between data attributes
     data_cov = data_nc.cov().round(3)
+    print('Problem 1b: Covariance and correlation pairs', file=file)
+    print('Covariance pairs table', file=file)
     print(data_cov.to_string(), end='\n\n', file=file)
     
+    # Calculate the correlation pairs between data attributes
     data_corr = data_nc.corr().round(3)
+    print('Correlation pairs table', file=file)
     print(data_corr.to_string(), end='\n\n', file=file)
     
-    data.hist(bins=10)
-    plt.savefig('histograms.eps', dpi=500)
+    # Plot data histograms
+    fig0, ax0 = plt.subplots()
+    ax_hist = data_nc.hist(bins=10, ax=ax0, histtype='step',
+        color='black', linewidth=2)
+    fig0.tight_layout()
+    fig0.subplots_adjust(wspace=0.15, hspace=0.4)
+    for ax in ax_hist.flatten():
+        ax.yaxis.set_major_locator(mtick.MultipleLocator(10))
+        ax.grid(False)
+    #plt.savefig('figs/histograms.eps', dpi=500)
+    print('Problem 1c: Histograms (see figs)', end='\n\n', file=file)
     
+    # Subset of attributes to make scatter plots of
     data_subset = data[['Age', 'BMI', 'Glucose', 'Insulin']]
     
-    pd.plotting.scatter_matrix(data_subset, c=data['Classification'])
-    plt.savefig('scatter_matrix.eps', dpi=500)
+    # Plot scatter plot pairs
+    cmap = plt.get_cmap('Set1')
+    plot_colors = mpl.colors.ListedColormap(cmap(np.arange(0, 2, 1)))
+    fig2, ax2 = plt.subplots()
+    pd.plotting.scatter_matrix(data_subset, c=data['Classification'],
+                               cmap=plot_colors, ax=ax2, 
+                               hist_kwds={'bins':10, 'histtype':'step',
+                                          'color':'black', 'linewidth':2})
+    fig2.tight_layout()
+    fig2.subplots_adjust(wspace=0, hspace=0)
+    #plt.savefig('figs/scatter_matrix.eps', dpi=500)
+    print('Problem 1e: Scatter Plot Matrix (see figs)', end='\n\n', file=file)
     
-    fig, ax = plt.subplots()
-    ax = pd.plotting.parallel_coordinates(data, 'Classification', color=('red', 'blue'),
+    # Plot parallel coordinates of data
+    fig3, ax3 = plt.subplots()
+    pd.plotting.parallel_coordinates(data, 'Classification', ax=ax3, color=('red', 'blue'),
                                           cols=['MCP.1', 'Adiponectin', 'Insulin', 'HOMA', 
                                                 'BMI', 'Age', 'Leptin', 'Glucose', 'Resistin'])
-    ax.set_xticklabels(ax.xaxis.get_majorticklabels(), rotation=45)
-    plt.tight_layout()
-    plt.savefig('parallel_coordinates.eps', dpi=500)
+    ax3.set_xticklabels(ax3.xaxis.get_majorticklabels(), rotation=45)
+    ax3.grid(False)
+    fig3.tight_layout()
+    #plt.savefig('figs/parallel_coordinates.eps', dpi=500)
+    print('Problem 1f: Parallel Coordinates Plot (see figs)', end='\n\n', file=file)
     
-    
+    # Generate an array of indices, shuffle them and then use the first
+    # 10 elements as the indices of the randomly sampled data points
     data_ind = np.arange(0, len(data), 1)
     np.random.shuffle(data_ind)
-    rand_sample = data.iloc[data_ind[:10]]
+    rand_sample = data.iloc[data_ind[:10]].round(3)
+    print('Problem 2a: Random sample of 10 data points', file=file)
+    print(rand_sample.to_string(), end='\n\n', file=file)
     
-    print(rand_sample.to_string(), file=file)
-    
+    # Perform PCA on the data and makes a scatter plot with the new 2 dimensions
     pca = PCA(n_components=2)
     pca.fit(data_nc.values)
-    
     data_trans = pca.transform(data_nc.values)
-    
-with open('latex_tables.txt', 'w+') as file2:
+    fig4, ax4 = plt.subplots()
+    pca_scat = ax4.scatter(data_trans[:,0], data_trans[:,1],
+                           c=data['Classification'], cmap=plot_colors)
+    ax4.add_artist(ax4.legend(pca_scat.legend_elements()[0], ('Healthy', 'Patient')))
+    fig4.tight_layout()
+    #plt.savefig('figs/pca_scatter.eps', dpi=500)
+    print('Problem 2b: PCA Scatter Plot (see figs)', end='\n\n', file=file)
+
+# Writes out table TeX code for report
+with open('etc/latex_tables.txt', 'w+') as file2:
     data_latex = data[data.keys()[:-1]]
     print(data_desc.to_latex(), end='\n\n', file=file2)
     print(data_cov.to_latex(), end='\n\n', file=file2)
