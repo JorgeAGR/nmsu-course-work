acd57bd43d8512fe3772c08b62707032136d778b
Jorge A. Garcia <jorgeagr97@gmail.com>
2019-10-14 18:05:29 -0600

    Update

--- GPHYS530/Project/make_arrays.py
+++ GPHYS530/Project/make_arrays.py
@@ -10,6 +10,19 @@ import os
 import numpy as np
 import obspy
 
+def sac2npy(directory, files):
+    seismos = np.zeros((len(files), data_points))
+    for i, file in enumerate(files):
+        print('File', i+1, '/', len(files))
+        seis = obspy.read(directory + file)
+        seis = seis[0].resample(resample_Hz).detrend()
+        seis.data = seis.data / np.abs(seis.data).max()
+        if seis.data.shape[0] > data_points:
+            seis.data = seis.data[1:]
+        seismos[i] = seis.data
+    
+    return seismos
+
 directories = ['data/', 'data/train/', 'data/test/']
 for d in directories:
     try:
@@ -25,16 +38,11 @@ test_files = np.sort(os.listdir(test_dir))
 resample_Hz = 10
 data_points = 5000
 
-def sac2npy(directory, files):
-    seismos = np.zeros(len(files), data_points)
-    for i, file in enumerate(files):
-        seis = obspy.read(train_dir + file)
-        seis = seis[0].resample(resample_Hz).detrend()
-        if seis.data.shape > data_points:
-            seis.data = seis.data[1:]
-        seismos[i] = seis.data
-    
-    return seismos
-
 train_seismos = sac2npy(train_dir, train_files)
 test_seismos = sac2npy(test_dir, test_files)
+
+train_seismos = train_seismos.reshape(train_seismos.shape[0], train_seismos.shape[1], 1)
+test_seismos = test_seismos.reshape(test_seismos.shape[0], test_seismos.shape[1], 1)
+
+np.save('data/train/train_seismos', train_seismos)
+np.save('data/test/test_seismos', test_seismos)
