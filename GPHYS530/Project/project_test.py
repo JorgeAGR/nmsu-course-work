443de5bb1ff3fd8d1fea961e8a1c5820ae035f05
Jorge A. Garcia <jorgeagr97@gmail.com>
2019-10-15 20:33:25 -0600

    Update

--- GPHYS530/Project/project_test.py
+++ GPHYS530/Project/project_test.py
@@ -10,67 +10,92 @@ import numpy as np
 import obspy
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from keras.layers import Input, Dense, Conv1D, MaxPooling1D, UpSampling1D, BatchNormalization, Reshape, Conv1DTranspose
+from keras.layers import Input, Dense, Conv1D, MaxPooling1D, UpSampling1D, BatchNormalization, Reshape, Flatten
 from keras.models import Model
+from keras.optimizers import Adam
 
+''''
+# This stumped me! I can't think recursively. Figure it out later.
+def get_Decoder_Output(autoencoder, compression_size, layer, i=1, layers=12):
+    if i == layers:
+        layer = autoencoder.layers[-i](layer)
+        decoder_input = Input(shape=(compression_size,))
+        return decoder_input, layer(decoder_input)
+    else:
+        return get_Decoder_Output(autoencoder, compression_size, layer, )
+'''
+'''
+    if layers >= i:
+        layer = autoencoder.layers[-i](get_Decoder_Output(autoencoder, compression_size, i+1, layers=layers))
+        return layer
+    else:
+        layer = autoencoder.layers[-i](get_Decoder_Output(autoencoder, compression_size, i+1, layers=layers))
+        decoder_input = Input(shape=(compression_size,))
+        return decoder_input, layer(decoder_input)
+'''
+def get_Decoder_Layers(autoencoder, layers=12):
+    
+    #expand = autoencoder.layers[-12]
+    decoder_layers = []
+    for i in range(1, layers+1):
+        decoder_layers.append(autoencoder.layers[-i])
+        
+    return decoder_layers
 
 def rossNet_CAE(input_length, compression_size):
     input_seis = Input(shape=(input_length, 1))
 
-    conv1_1 = Conv1D(32, kernel_size=21, strides=1,
-                     activation='relu')(input_seis)
-    conv1_2 = Conv1D(32, kernel_size=21, strides=1,
-                     activation='relu')(conv1_1)
-    bn1 = BatchNormalization()(conv1_2)
+    conv1 = Conv1D(32, kernel_size=21, strides=1,
+                     activation='relu', padding='same')(input_seis)
+    bn1 = BatchNormalization()(conv1)
     max1 = MaxPooling1D(pool_size=2)(bn1)
 
-    conv2_1 = Conv1D(64, kernel_size=15, strides=1,
-                     activation='relu')(max1)
-    conv2_2 = Conv1D(64, kernel_size=15, strides=1,
-                     activation='relu')(conv2_1)
-    bn2 = BatchNormalization()(conv2_2)
+    conv2 = Conv1D(64, kernel_size=15, strides=1,
+                     activation='relu', padding='same')(max1)
+    bn2 = BatchNormalization()(conv2)
     max2 = MaxPooling1D(pool_size=2)(bn2)
 
-    conv3_1 = Conv1D(128, kernel_size=11, strides=1,
-                     activation='relu')(max2)
-    conv3_2 = Conv1D(128, kernel_size=11, strides=1,
-                     activation='relu')(conv3_1)
-    bn3 = BatchNormalization()(conv3_2)
+    conv3 = Conv1D(128, kernel_size=11, strides=1,
+                     activation='relu', padding='same')(max2)
+    bn3 = BatchNormalization()(conv3)
     max3 = MaxPooling1D(pool_size=2)(bn3)
 
     flattened = Flatten()(max3)
     
     encoding = Dense(compression_size, activation='relu')(flattened)
     
-    reshaped = Reshape(max3.output_shape)(encoding)
+    expand = Dense(max3.shape.as_list()[1] * max3.shape.as_list()[2], activation='relu')(encoding)
+    
+    reshaped = Reshape(max3.shape.as_list()[1:])(expand)
+    
+    convup1 = Conv1D(128, kernel_size=11, strides=1,
+                     activation='relu', padding='same')(reshaped)
+    bn_up1 = BatchNormalization()(convup1)
+    up1 = UpSampling1D(size=2)(bn_up1)
+    
+    convup2 = Conv1D(64, kernel_size=15, strides=1,
+                     activation='relu', padding='same')(up1)
+    bn_up2 = BatchNormalization()(convup2)
+    up2 = UpSampling1D(size=2)(bn_up2)
+    
+    convup3 = Conv1D(32, kernel_size=21, strides=1,
+                     activation='relu', padding='same')(up2)
+    bn_up3 = BatchNormalization()(convup3)
+    up3 = UpSampling1D(size=2)(bn_up3)
+    
+    decoding = Conv1D(1, kernel_size=21, strides=1,
+                     activation='sigmoid', padding='same')(up3)
+    
+    autoencoder = Model(input_seis, decoding)
+    
+    decoder_input = Input(shape=(compression_size,))
+    decoder_layers = get_Decoder_Layers(autoencoder)
+    
+    encoder = Model(input_seis, encoding)
+    decoder = Model(decoder_input,
+                    decoder_layers[0](decoder_layers[1](decoder_layers[2](decoder_layers[3](decoder_layers[4](decoder_layers[5](decoder_layers[6](decoder_layers[7](decoder_layers[8](decoder_layers[9](decoder_layers[10](decoder_layers[11](decoder_input)))))))))))))
 
-    up1 = UpSampling1D(size=2)(reshaped)
-    bn_up1 = BatchNormalization()(up1)
-    convup1_1 = Conv1DTranspose(128, kernel_size=11, strides=1,
-                     activation='relu')(bn_up1)
-    convup1_2 = Conv1DTranspose(128, kernel_size=11, strides=1,
-                     activation='relu')(convup1_1)
+    autoencoder.compile(loss='binary_crossentropy',#huber_loss,
+                  optimizer=Adam(1e-3))
 
-    up2 = UpSampling1D(size=2)(convup1_2)
-    bn_up2 = BatchNormalization()(up2)
-    convup2_1 = Conv1DTranspose(64, kernel_size=15, strides=1,
-                     activation='relu')(bn_up2)
-    convup2_2 = Conv1DTranspose(64, kernel_size=15, strides=1,
-                     activation='relu')(convup2_1)
-
-    up3 = UpSampling1D(size=2)(convup2_2)
-    bn_up3 = BatchNormalization()(up3)
-    convup3_1 = Conv1DTranspose(32, kernel_size=21, strides=1,
-                     activation='relu')(bn_up3)
-    convup3_2 = Conv1DTranspose(32, kernel_size=21, strides=1,
-                     activation='relu')(convup3_1)
-    output = Conv1D(1, kernel_size=21, strides=1,
-                     activation='rely')(convup3_2)
-
-    model = Model(inputs=input_spectra, outputs=[col_dense[-1], temp[-1],
-                                                 density[-1], ionized[-1], metal[-1]])
-
-    model.compile(loss='mean_squared_error',#huber_loss,
-                  optimizer=Adam(1e-5/2))
-
-    return model)
+    return autoencoder, encoder, decoder
