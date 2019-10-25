527e699dd2e19a27d9f4bba8c8fd9aad4776a778
Jorge A. Garcia <jorgeagr97@gmail.com>
2019-10-24 20:37:37 -0600

    Update

--- CS508/HW3_Garcia/prob2_logreg_roc.py
+++ CS508/HW3_Garcia/prob2_logreg_roc.py
@@ -42,21 +42,38 @@ y = data.loc[:, 4].values
 # Split into 80/20 Train/Test sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
+# Fit a Logistic Regression model to the training data
 log_reg = LogisticRegression(solver='liblinear', random_state=0)
 log_reg.fit(x_train, y_train)
-train_score = log_reg.score(x_train, y_train)
-test_score = log_reg.score(x_test, y_test)
 
+# Predict class probability for the testing set and evaluate the ROC curve
 test_prob = log_reg.predict_proba(x_test)
 fpr, tpr, thresholds = roc_curve(y_test, test_prob[:,1])
 
-'''
-cmap = plt.get_cmap('Set1')
-plot_colors = mpl.colors.ListedColormap(cmap(np.arange(0, 2, 1)))
-fig_scat, ax_scat = plt.subplots()
-pd.plotting.scatter_matrix(data.iloc[:,:4], c=data.iloc[:,4],
-                           cmap=plot_colors, ax=ax_scat, 
-                           hist_kwds={'bins':10, 'histtype':'step',
-                                      'color':'black', 'linewidth':2})
-fig_scat.tight_layout(h_pad=0)
-'''
+# Plotting routine for ROC
+fig, ax = plt.subplots()
+ax.plot(fpr, tpr, color='blue', label='Setosa ROC')
+ax.plot(np.linspace(0, 1), np.linspace(0, 1), '--', color='black', label='Chance')
+ax.xaxis.set_major_locator(mtick.MultipleLocator(0.5))
+ax.xaxis.set_minor_locator(mtick.MultipleLocator(0.1))
+ax.yaxis.set_major_locator(mtick.MultipleLocator(0.5))
+ax.yaxis.set_minor_locator(mtick.MultipleLocator(0.1))
+ax.set_xlim(-0.05, 1.05)
+ax.set_ylim(-0.05, 1.05)
+ax.legend()
+plt.savefig('figs/prob2_logreg_roc.eps', dpi=500)
+
+
+# Appendix Code: Plot the scatter plot matrix of the data set; I was skpetical
+# about the perfect classification but the classes are linearly seperable.
+plot_scatter = False
+if plot_scatter:
+    cmap = plt.get_cmap('tab10')
+    plot_colors = mpl.colors.ListedColormap(cmap([0, 3]))
+    fig_scat, ax_scat = plt.subplots()
+    pd.plotting.scatter_matrix(data.iloc[:,:4], c=data.iloc[:,4],
+                               cmap=plot_colors, ax=ax_scat, 
+                               hist_kwds={'bins':10, 'histtype':'step',
+                                          'color':'black', 'linewidth':2})
+    fig_scat.tight_layout(h_pad=0, w_pad=0)
+    plt.savefig('figs/prob2_scatter.eps', dpi=500)
