f513909597fd8694c9fd4ed68fa332b0d9212071
Jorge A. Garcia <jorgeagr97@gmail.com>
2019-11-11 14:06:52 -0700

    Update

--- CS508/HW4_Garcia/prob2_kmeans.py
+++ CS508/HW4_Garcia/prob2_kmeans.py
@@ -8,9 +8,25 @@ Created on Sun Nov 10 18:05:31 2019
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 
 data = pd.read_csv('data.csv')
 
+cluster_sizes = np.arange(2, 21, 1)
+silh_per_clusters = np.zeros((len(cluster_sizes),2))
+for k_clusters in cluster_sizes:
+    trials = 10
+    silh_coeffs = np.zeros(trials)
+    for i in range(trials):
+        kmeans = KMeans(n_clusters=k_clusters, init='random', n_init=1, random_state=i)
+        kmeans.fit(data.values)
+        silh_coeffs[i] = silhouette_score(data.values, kmeans.labels_)
+    silh_per_clusters[k_clusters-2,0] = silh_coeffs.mean()
+    silh_per_clusters[k_clusters-2,1] = silh_coeffs.std()
+    
+fig, ax = plt.subplots(nrows=2)
+ax[0].errorbar(cluster_sizes, silh_per_clusters[:,0], yerr=silh_per_clusters[:,1], capsize=5,
+               marker='o', markersize=5, markeredgewidth=2, color='black')
