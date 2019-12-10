efcc337be27574c020c2dccd8144ccf721475bbd
Jorge A. Garcia <jorgeagr97@gmail.com>
2019-12-09 17:47:03 -0700

    Update

--- EE565/Project6_GarciaJ/codes/prob2.py
+++ EE565/Project6_GarciaJ/codes/prob2.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
@@ -47,43 +48,56 @@ criterion_list = ['entropy', 'gini']
 colors = ['indianred', 'royalblue', 'lightcoral', 'cornflowerblue']
 highest_acc = 0
 for c, criterion in enumerate(criterion_list):
-    num_tree_acc = np.zeros((len(max_trees), 2))
+    num_tree_accval = np.zeros((len(max_trees), 2))
+    num_tree_tpr = np.zeros((len(max_trees), 2))
+    num_tree_tnr = np.zeros((len(max_trees), 2))
     for t in max_trees:
-        train_acc = np.zeros(folds)
         val_acc = np.zeros(folds)
+        val_tpr = np.zeros(folds)
+        val_tnr = np.zeros(folds)
         for i, inds in enumerate(kfold.split(x_train)):
             train_ind = inds[0]
             val_ind = inds[1]
             tree = DecisionTreeClassifier(criterion=criterion, max_depth=t, random_state=0)
             tree.fit(x_train, y_train)
-            train_acc[i] = tree.score(x_train[train_ind], y_train[train_ind])
             val_acc[i] = tree.score(x_train[val_ind], y_train[val_ind])
+            # Confusion Matrix considers the case vote = 1 as positive,
+            # so considers voting for Rep
+            tn, fp, fn, tp = confusion_matrix(y_train[val_ind], tree.predict(x_train[val_ind])).ravel()
+            val_tpr[i] = tp / (tp + fn)
+            val_tnr[i] = tn / (tn + fp)
             if val_acc[i] > highest_acc:
                 # save the best tree (highest acc)
-                highest_acc = val_acc[i]
                 best_tree = tree
-        num_tree_acc[t-1, 0] = train_acc.mean()
-        num_tree_acc[t-1, 1] = train_acc.std()
-    acc_mean = num_tree_acc[:,0]
-    acc_std = num_tree_acc[:,1]
-    ax.plot(max_trees, acc_mean, color=colors[c], label=criterion, zorder=1)
-    ax.fill_between(max_trees, acc_mean - acc_std, acc_mean + acc_std, color=colors[c+2], zorder=0)
+                highest_acc = val_acc[i]
+        num_tree_accval[t-1,0] = val_acc.mean()
+        num_tree_accval[t-1,1] = val_acc.std()
+        num_tree_tpr[t-1,0] = val_tpr.mean()
+        num_tree_tpr[t-1,1] = val_tpr.std()
+        num_tree_tnr[t-1,0] = val_tnr.mean()
+        num_tree_tnr[t-1,1] = val_tnr.std()
+        
+    acc_meanval = num_tree_accval[:,0]
+    acc_stdval = num_tree_accval[:,1]
+    ax.plot(max_trees, acc_meanval, color=colors[c], label=criterion, zorder=1)
 ax.xaxis.set_major_locator(mtick.MultipleLocator(25))
 ax.xaxis.set_minor_locator(mtick.MultipleLocator(5))
 ax.yaxis.set_major_locator(mtick.MultipleLocator(0.05))
 ax.yaxis.set_minor_locator(mtick.MultipleLocator(0.01))
 ax.set_xlim(0, 150)
 ax.set_ylim(0.57, 0.86)
-ax.set_xlabel('Number of trees')
-ax.set_ylabel('Accuracy')
+ax.set_xlabel('Tree Depth')
+ax.set_ylabel('Validation Accuracy')
 ax.legend(loc='lower right')
 fig.tight_layout(pad=0.5)
 fig.savefig('../prob2.eps', dpi=500)
 
-print('Optimal performance with {} criterion, {} trees with {:.2f} +/- {:.2f}% accuracy'.format(best_tree.criterion,
+print('Optimal performance with {} criterion, {} trees with {:.2f} +/- {:.2f}% acc, {:.2f} +/- {:.2f}% tpr, {:.2f} +/- {:.2f}% tnr'.format(best_tree.criterion,
           best_tree.max_depth,
-          num_tree_acc[best_tree.max_depth-1][0]*100,
-          num_tree_acc[best_tree.max_depth-1][1]*100))
+          num_tree_accval[best_tree.max_depth-1][0]*100, num_tree_accval[best_tree.max_depth-1][1]*100,
+          num_tree_tpr[best_tree.max_depth-1][0]*100, num_tree_tpr[best_tree.max_depth-1][1]*100,
+          num_tree_tnr[best_tree.max_depth-1][0]*100, num_tree_tnr[best_tree.max_depth-1][1]*100))
+          #best_precision*100, best_recall*100))
 
 # load testing set to predict election results
 data_test = pd.read_csv('../data/election_data_test.csv')
